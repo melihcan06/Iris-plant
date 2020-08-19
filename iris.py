14264acb4998167e1d22468a164acaf830031214
Melih Can <33932031+melihcan06@users.noreply.github.com>
2020-08-19 09:17:23 +0300

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,25 +1,21 @@
-import keras as kr
+import tensorflow.keras as kr
 import numpy as np
 
-from sklearn.preprocessing import Imputer
 import pandas as pd
 
-veri=pd.read_csv("iris.data")
+veri=pd.read_csv("C:/Users/user/Downloads/Neural-Network-Example-master/iris.data")
 
 veri=veri.replace("Iris-setosa",0)
 veri=veri.replace("Iris-versicolor",1)
 veri=veri.replace("Iris-virginica",2)
 
-imp=Imputer()
-veri=imp.fit_transform(veri)
-
-giris=veri[:,0:4]
-cikis=veri[:,4:]
+giris=veri.iloc[:,0:4]
+cikis=veri.iloc[:,4:]
 
 model=kr.Sequential()
 model.add(kr.layers.Dense(64,input_dim=4))
 model.add(kr.layers.Activation("relu"))
-model.add(kr.layers.Dense(64))
+model.add(kr.layers.Dense(3))
 model.add(kr.layers.Activation("softmax"))
 
 model.compile(optimizer=kr.optimizers.Adadelta(),loss="sparse_categorical_crossentropy",metrics=["accuracy"])
